52318e84471224e8a8bcd89ff2906910bd42c11d
babu <karanamseshubabu28@gmail.com>
2024-03-22 20:00:27 +0530

    added

--- src/test/java/com/cucumber/hooks/Hooks.java
+++ src/test/java/com/cucumber/hooks/Hooks.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.cucumber.pageObjects.LoginPage;
 
@@ -36,7 +37,7 @@ public class Hooks {
 		if (br.equalsIgnoreCase("chrome")) {
 			driver = new ChromeDriver();
 		} else if (br.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
+			driver = new FirefoxDriver();
 		}
 		return driver;
 	}
